8b5c86480014e279cdf2e311862d509cd153c350
Masanori Kato <nitaka.masa@gmail.com>
2012-04-13 00:19:59 +0900

    1st commit

---
+++ src/oit/is/sclab/testproject/TestClassMain.java
@@ -0,0 +1,13 @@
+package oit.is.sclab.testproject;
+
+public class TestClassMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Test Projectです");
+	}
+
+}
